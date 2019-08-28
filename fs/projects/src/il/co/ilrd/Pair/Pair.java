2ab19626e95c167e8e4dda6d277e1caa94ddac21
dani.benjamin <danibenjamin46@gmail.com>
2019-08-28 15:25:07 +0300

    Wednesday

---
+++ fs/projects/src/il/co/ilrd/Pair/Pair.java
@@ -0,0 +1,134 @@
+package il.co.ilrd.Pair;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+
+
+public class Pair <K, V> implements Map.Entry<K, V>{
+
+	private K key;
+	private V value;
+		
+	private Pair(K key, V value){
+		this.key = key;
+		this.value = value;
+	}
+	
+	public static <K, V> Pair<K, V> of(K key, V value){
+		return new Pair<K, V>(key, value);
+	}
+	
+	public static <K, V> Pair<V, K> swap(Pair<K, V> pair){
+		return new Pair<V, K>(pair.value, pair.key);
+	}
+	
+	public static <T extends Comparable <T>> Pair<T, T> minMax(T[] elements){
+		
+		class compareClass implements Comparator<T> 
+		{ 
+			@Override
+			public int compare(T a, T b) 
+		    { 
+		        return a.compareTo(b); 
+		    } 
+		}
+		return minMax(elements, new compareClass());
+	}
+	
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/	
+	
+	
+	private static <T> Pair<T, T> comparePair(T left, T right, Comparator <T> c)
+	{
+		if(c.compare(left, right) > 0)
+		{
+			return new Pair<T,T>(right, left);
+		}
+		return new Pair<T,T>(left, right);
+	}
+	
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/		
+	
+	public static <T> Pair<T, T> minMax(T[] elements, Comparator <T> c){
+		T min = elements[0];
+		T max = elements[0];
+		int i = 0;
+		List<Pair<T,T>> pairs = new ArrayList<Pair<T,T>>();
+		
+		
+		for(; i < elements.length - 2; i += 2)
+		{
+			pairs.add(comparePair(elements[i], elements[i + 1], c));
+		}
+		
+		if(elements.length % 2 != 0)
+		{
+			pairs.add(comparePair(elements[i], elements[i], c));
+		}
+		
+		else
+		{
+			pairs.add(comparePair(elements[i], elements[i + 1], c));
+		}
+		
+		for(Pair<T,T> p : pairs)
+		{
+			if(c.compare(p.key, min) < 0)
+			{
+				min = p.key;
+			}
+			if(c.compare(p.value, max) > 0)
+			{
+				max = p.value;
+			}
+		}
+		
+		return new Pair<T,T>(min, max);
+	}
+	
+	@Override
+	public K getKey() {
+
+		return key;
+	}
+	
+	public K setKey(K newKey){
+		K tmpkey = newKey;
+		key = tmpkey;
+		return tmpkey;
+	}
+
+	@Override
+	public V getValue() {
+		return value;
+	}
+	
+	@Override
+	public V setValue(V newValue) {
+		V tmpVal = value;
+		value = newValue;
+		return tmpVal;
+	}
+
+	@Override
+	public String toString() {
+		return ("key: " + key + ", value: " + value) ;
+	}
+
+	@Override
+	public int hashCode() {
+		int result = 17;
+        result = 31 * result + key.hashCode();
+        result = 31 * result + value.hashCode();
+        return result;
+	}
+	
+	@Override
+	@SuppressWarnings("unchecked")
+	public boolean equals(Object obj) {
+		return (key == ((Pair<K,V>)obj).key && value == ((Pair<K,V>)obj).value);
+	}
+}
+
---
+++ fs/projects/src/il/co/ilrd/Pair/PairTest.java
@@ -0,0 +1,76 @@
+package il.co.ilrd.Pair;
+
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.*; 
+
+import org.junit.jupiter.api.Test;
+
+class PairTest {
+
+	@Test
+	void setNGetTest() {
+		String firstName = "dani";
+		Integer age = 26;
+		String anotherFirstName = "roni";
+		Pair <String,Integer> name = Pair.of(firstName, age);
+		
+		assertEquals(firstName, name.getKey(), "failed get key");
+		assertEquals(age, name.getValue(), "failed get value");
+		
+		name.setKey(anotherFirstName);
+		assertEquals(anotherFirstName, name.getKey(), "failed set key");
+	}
+	@SuppressWarnings("unchecked")
+	@Test
+	void minMaxComparatorTest() {
+		String firstName1 = "dani";
+		Integer age1 = 26;
+		String firstName2 = "roni";
+		Integer age2 = 27;
+		String firstName3 = "oren";
+		Integer age3 = 33;
+		
+		Pair <String,Integer> name1 = Pair.of(firstName1, age1);
+		Pair <String,Integer> name2 = Pair.of(firstName2, age2);
+		Pair <String,Integer> name3 = Pair.of(firstName3, age3);
+		
+		Pair[] pairArray = {name1, name2, name3};
+		
+		Pair <String,Integer> minMax = Pair.minMax(pairArray, new comparePair());
+		
+		assertEquals(minMax.getKey(), name1, "failed min max with comperator");
+		assertEquals(minMax.getValue(), name3, "failed min max with comperator");
+	}
+	
+	@Test
+	void minMaxComparableTest() {	
+		Integer[] intArray = {1, 0, 8, 5, 7, 9, 10, 2, -1, -8, 11};
+		
+		Pair <Integer,Integer> minMax = Pair.minMax(intArray);
+		assertEquals(minMax.getKey(), -8, "failed min with comperator");
+		assertEquals(minMax.getValue(), 11, "failed max with comperator");
+		
+	}
+	
+	@Test
+	void swapTest() {	
+		String firstName = "dani";
+		Integer age = 26;
+		Pair <String,Integer> name = Pair.of(firstName, age);
+		Pair <Integer, String> swappedName = Pair.swap(name);
+		
+		assertEquals(swappedName.getKey(), age, "failed swap");
+		assertEquals(swappedName.getValue(), firstName, "failed swap");
+	}
+}
+
+
+/*-------------------------------------------------------------------*/
+class comparePair implements Comparator<Pair<String, Integer>> 
+{ 
+	@Override
+    public int compare(Pair<String,Integer> a, Pair<String,Integer> b) 
+    { 
+        return ((Integer)a.getValue() - (Integer)b.getValue()); 
+    } 
+} 
